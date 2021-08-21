ad9aed6a231d1b7ec00a256ae5cf55f85845fcce
dejan-o <dejan.obr@hotmail.com>
2021-08-21 23:15:28 +0200

    creating styles, config file, reset css, site container

--- citrus-app/src/index.js
+++ citrus-app/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
+import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
